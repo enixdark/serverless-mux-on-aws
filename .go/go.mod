48ce351282642efc83426a6b8d8b2f9ea0c86acf
Pahud <pahudnet@gmail.com>
2021-07-16 22:56:45 +0800

    Initial commit

---
+++ .go/go.mod
@@ -0,0 +1,9 @@
+module github.com/pahud/serverless-mux-go/m/v2
+
+go 1.16
+
+require (
+	github.com/aws/aws-lambda-go v1.25.0
+	github.com/awslabs/aws-lambda-go-api-proxy v0.10.0
+	github.com/gorilla/mux v1.8.0
+)
---
+++ .go/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
+	"github.com/gorilla/mux"
+)
+
+const (
+	DEFAULT_SERVICE_NAME string = "mux"
+	DEFAULT_VERSION      string = "1.0"
+)
+
+var r *mux.Router
+var adapter *gorillamux.GorillaMuxAdapter
+
+func init() {
+	r = mux.NewRouter()
+	adapter = gorillamux.New(r)
+}
+
+func main() {
+	if isInLambda() {
+		lambda.Start(LambdaHandler)
+	} else {
+		r.HandleFunc("/", HomeHandler)
+		listenOn := "0.0.0.0:8080"
+		log.Printf("Start listening on http://%v", listenOn)
+		log.Fatal(http.ListenAndServe(listenOn, r))
+	}
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	serviceName := getEnvOrDefault("serviceName", DEFAULT_SERVICE_NAME)
+	versionNum := getEnvOrDefault("versionNum", DEFAULT_VERSION)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"serviceName": serviceName, "versionNum": versionNum})
+}
+
+func LambdaHandler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return adapter.ProxyWithContext(context.Background(), req)
+}
+
+func getEnvOrDefault(e string, d string) string {
+	if v, ok := os.LookupEnv(e); ok {
+		return v
+	}
+	return d
+}
+
+func isInLambda() bool {
+	return os.Getenv("AWS_LAMBDA_RUNTIME_API") != ""
+}
